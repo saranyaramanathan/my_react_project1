aef6202d4863960fd4dade60c42c48569c204b57
Saranya <saranya@credentech.net>
2025-06-04 23:22:39 +0530

    logo change

--- src/App.jsx
+++ src/App.jsx
@@ -56,17 +56,13 @@ const [itemCounter, setCounter] = useState(0);
       <nav className='sticky bg-gray-50 shadow-md'>
         <div className="flex justify-between items-center px-4 py-4 max-w-7xl mx-auto">
 
-          <img
-            width="50"
-            src="/src/assets/shop-icon.png"
-            alt=""
-          />
+         <svg xmlns="http://www.w3.org/2000/svg" id="logo-85" width="36"  viewBox="0 0 40 40" fill="none"><path class="ccustom" fill-rule="evenodd" clip-rule="evenodd" d="M10 0C15.5228 0 20 4.47715 20 10V0H30C35.5228 0 40 4.47715 40 10C40 15.5228 35.5228 20 30 20C35.5228 20 40 24.4772 40 30C40 32.7423 38.8961 35.2268 37.1085 37.0334L37.0711 37.0711L37.0379 37.1041C35.2309 38.8943 32.7446 40 30 40C27.2741 40 24.8029 38.9093 22.999 37.1405C22.9756 37.1175 22.9522 37.0943 22.9289 37.0711C22.907 37.0492 22.8852 37.0272 22.8635 37.0051C21.0924 35.2009 20 32.728 20 30C20 35.5228 15.5228 40 10 40C4.47715 40 0 35.5228 0 30V20H10C4.47715 20 0 15.5228 0 10C0 4.47715 4.47715 0 10 0ZM18 10C18 14.4183 14.4183 18 10 18V2C14.4183 2 18 5.58172 18 10ZM38 30C38 25.5817 34.4183 22 30 22C25.5817 22 22 25.5817 22 30H38ZM2 22V30C2 34.4183 5.58172 38 10 38C14.4183 38 18 34.4183 18 30V22H2ZM22 18V2L30 2C34.4183 2 38 5.58172 38 10C38 14.4183 34.4183 18 30 18H22Z" fill="#1447e6"/></svg>
           <h1 className="text-4xl text-blue-700">Friendly Store</h1>
           <div className="relative py-2 ">
-            <div className="t-0 absolute left-3">
+            <div className="t-0 absolute left-4">
               <p className="flex h-2 w-2 items-center justify-center rounded-full bg-red-500 p-3 text-xs text-white">{cartitems.length}</p>
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="file: mt-4 h-6 w-6" onClick={()=>{setVisible(true)}}>
+            <svg xmlns="http://www.w3.org/2000/svg"   fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="file: mt-4 h-8 w-8" onClick={()=>{setVisible(true)}}>
               <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"  />
             </svg>
           </div>
